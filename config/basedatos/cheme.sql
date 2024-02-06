0456097e92c488c1cb21fe1d2489fe2bed169177
ErikSanzana <ersanzana@gmail.com>
2024-02-06 10:52:20 -0300

    cambios realizados

--- config/basedatos/cheme.sql
+++ config/basedatos/cheme.sql
@@ -4,7 +4,6 @@ CREATE DATABASE softjobs;
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
--- saque referencias de buenas practidas... aun que por el desafio no puedo separar la contraseña en otra tabla  como lo sugeria...  
 CREATE TABLE usuarios (
     pk_id        SERIAL      PRIMARY KEY UNIQUE,
     id           uuid DEFAULT uuid_generate_v4() NOT NULL UNIQUE,
@@ -15,9 +14,5 @@ CREATE TABLE usuarios (
     created_at   TIMESTAMP   NOT NULL DEFAULT NOW(),
     updated_at   TIMESTAMP   NOT NULL DEFAULT NOW()
 );
-
---test
-INSERT INTO usuarios (email ,password, rol, lenguage) VALUES ( 1, '2', 3, '4') returning *; 
-
 
 SELECT * FROM usuarios;
